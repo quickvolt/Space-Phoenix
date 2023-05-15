31032acddbb8081beac13e59100e67b16f750137
aligoktas404 <121170357+aligoktas404@users.noreply.github.com>
2023-05-15 22:06:46 +0300

    script.js saat ayarı yenilendi

--- space-phoenix/script.js
+++ space-phoenix/script.js
@@ -5,11 +5,15 @@ function updateSaat() {
     var saat = tarih.getHours();
     var dakika = tarih.getMinutes();
     var saniye = tarih.getSeconds();
-    
-    saatDiv.textContent = saat + ':' + dakika + ':' + saniye;
+    saat = ("0" + saat).slice(-2);
+  
+    dakika = ("0" + dakika).slice(-2);
+    saniye = ("0" + saniye).slice(-2);
+  
+    saatDiv.textContent=saat + ':' + dakika + ':' + saniye;
   }
   
-  setInterval(updateSaat, 1000);
+  setInterval(updateSaat, 40);
 
 // Sayfa yüklenince animasyon başlatılır
 window.onload = function() {
