431a460dac710174037da14cfdf8a655c44028e9
Emin <123815184+Ragnar0011@users.noreply.github.com>
2023-06-09 00:39:32 +0300

    Delete Space-Phoenix-main-commit14 directory

--- Space-Phoenix-main-commit14/space-phoenix/hamburger.js
+++
@@ -1,8 +0,0 @@
-const navigation = document.getElementById("nav");
-const menu = document.getElementById("menu");
-
-menu.addEventListener("click", () => {
-  navigation.style.setProperty("--childenNumber", navigation.children.length);
-  navigation.classList.toggle("active");
-  menu.classList.toggle("active");
-});
